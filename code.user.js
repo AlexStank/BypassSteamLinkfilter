9cf31247b0e8098672ea662f775feb520c7aa5b6
Alexander Stankovich <65312732+AlexStank@users.noreply.github.com>
2025-07-19 12:03:13 +0300

    added community page support

--- code.user.js
+++ code.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         Обход linkfilter в Steam
 // @namespace    bypass_steam_linkfilter
-// @version      1.0
+// @version      1.1
 // @description  Обход подтверждения перехода по ссылке в форуме Steam
 // @author       AlexStank
+// @match        https://store.steampowered.com/*
 // @match        https://steamcommunity.com/*
 // @grant        none
 // ==/UserScript==
-
+ 
 (function() {
-    let links = document.getElementsByClassName('bb_link');
+    let links = document.getElementsByClassName(`${(document.location.href.includes('store')) ? 'linkbar' : 'bb_link'}`);
     for (let link of links) {
         if (link.href.includes('linkfilter')) link.href = link.href.split('?url=').pop();
     }
